cffd4604f2708aef2139cee9d3d5c42a704fd468
TYNYSTANOFF <tynystanov06@gmail.com>
2023-02-03 18:53:48 +0600

    creating

---
+++ app/src/main/java/com/example/aibolotproject/App.kt
@@ -0,0 +1,6 @@
+package com.example.aibolotproject
+
+import android.app.Application
+
+class App : Application(){
+}
---
+++ app/src/main/java/com/example/aibolotproject/presentation/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.aibolotproject.presentation.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aibolotproject.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
